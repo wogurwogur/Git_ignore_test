9b51a315a4ad65b38f7f81ab91bba893b106be21
최재혁 <wogur8531@naver.com>
2024-12-13 09:29:37 +0900

    ignore 연습

---
+++ Git_ignore_test/src/main/java/nocommit_some/NoCommit_1.java
@@ -0,0 +1,10 @@
+package nocommit_some;
+
+public class NoCommit_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Git_ignore_test/src/main/java/nocommit_some/NoCommit_2.java
@@ -0,0 +1,10 @@
+package nocommit_some;
+
+public class NoCommit_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
